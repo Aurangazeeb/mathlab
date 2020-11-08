dceefe6dce8af8c475f418ca5a5746eddf5c4538
Aurangazeeb A K <aurangazeebak@gmail.com>
2020-11-08 23:01:49 +0530

    organized files

---
+++ taylor.py
@@ -0,0 +1,22 @@
+import numpy as np
+from mathutils import operators as ops
+
+def taylor_series(nterms, indep_var, basefunc, x_val, origin = 0):
+  baseval = basefunc.evalf(n =3, subs = {indep_var : origin})
+  # print('baseval :', baseval)
+  taylor_terms = [baseval]
+  for i in range(1, nterms):
+    basefunc = basefunc.diff(indep_var)
+    # print(dict(basefunc = basefunc))
+    func_at_origin = basefunc.evalf(n=3 , subs = {indep_var : origin})
+    # yprime_vals = [baseval]
+    xoffset =  x_val - origin
+    var_terms = xoffset**i
+    denom = ops.factorial(i)
+    taylor_terms.append((func_at_origin * var_terms)/denom)
+  return taylor_terms
+
+def taylor_val(series, indep_var, min = -30, max = 30, precision = 3):
+  taylor_func = sum(series)
+  taylor_vals = [taylor_func.evalf(n = precision, subs = {indep_var : val}) for val in np.arange(min, max +1)]
+  return taylor_vals
--- taylor_plot.py
+++ taylor_plot.py
@@ -2,49 +2,26 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import sympy as sp
-
-
-def factorial(n):
-  val = 1
-  for i in range(1, n+1):
-    val *= i
-  return val
+# from mathutils import operators as ops
+import taylor
 
 x = sp.Symbol('x')
 num_pow = 2
-y = 2 /(x**num_pow - x)
+y = 5*x**2 + 23*x**5 + 3*x**8
+
+
+# 2 /(x**num_pow - x)
 
 #5*x**2 + (x**3) - 34*x**-2
 #
 
 baseline = [y.evalf(n =3, subs = dict(x = val)) for val in np.arange(-30, 30)]
 
-
-def taylor_series(nterms, indep_var, basefunc, x_val, origin = 0):
-  baseval = basefunc.evalf(n =3, subs = {indep_var : origin})
-  # print('baseval :', baseval)
-  taylor_terms = [baseval]
-  for i in range(1, nterms):
-    basefunc = basefunc.diff(indep_var)
-    # print(dict(basefunc = basefunc))
-    func_at_origin = basefunc.evalf(n=3 , subs = {indep_var : origin})
-    # yprime_vals = [baseval]
-    xoffset =  x_val - origin
-    var_terms = xoffset**i
-    denom = factorial(i)
-    taylor_terms.append((func_at_origin * var_terms)/denom)
-  return taylor_terms
-
-def taylor_val(series, min = -30, max = 30, precision = 3):
-  taylor_func = sum(series)
-  taylor_vals = [taylor_func.evalf(n = precision, subs = {x : val}) for val in np.arange(min, max +1)]
-  return taylor_vals
-
 ref_point = 30
 num_terms = 1
-taylorseries = taylor_series(num_terms, x, y, x, ref_point)
+taylorseries = taylor.taylor_series(num_terms, x, y, x, ref_point)
 
-taylor_value = taylor_val(taylorseries)
+taylor_value = taylor.taylor_val(taylorseries, x)
 plt.plot(baseline, label = 'baseline', linestyle = '--', lw = 5)
 plt.plot(taylor_value, label = 'taylor')
 
@@ -53,8 +30,8 @@ def animate(interval):
   global num_terms
   num_terms += 1
   print(f'Approximating to {num_terms} taylor terms')
-  taylorseries = taylor_series(num_terms, x, y, x, ref_point)
-  taylor_value = taylor_val(taylorseries)
+  taylorseries = taylor.taylor_series(num_terms, x, y, x, ref_point)
+  taylor_value = taylor.taylor_val(taylorseries, x)
   plt.plot(baseline, label = 'baseline', linestyle = '--', lw = 5);plt.plot(taylor_value, label = 'taylor')
   #plt.xlim(-10,90); 
   #plt.ylim(-100, 100)
